ce415745c185b20c80861493f8bef45a69f84394
Sean <you@example.com>
2021-05-26 09:14:42 +0000

    added some ascii art

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
+import art
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 def shifting(letter, value, process):
     if process == 1:
-        if (letter + value) > 25:
-            return alphabet[(letter+value) - 26]
-        else:
-            return alphabet[letter + value]
+        return alphabet[(letter+value) % 26]
     elif process == 2:
         if (letter - value) < 0:
             return alphabet[(letter - value) + 26]
@@ -28,14 +27,21 @@ def decrypt(text, shift):
         final.append(end)
     return "".join(final)
 
+print(art.logo)
+
 while True:
-    direction = input("\nType 'encode' to encrypt, type 'decode' to decrypt:\n")
+
+    while True:
+        direction = input("\nType 'encode' to encrypt, type 'decode' to decrypt:\n")
+        if (direction == "encode") or (direction == "decode"):
+            break
+    
     text = input("Type your message:\n").lower()
     shift = int(input("Type the shift number:\n"))
 
     if direction == "encode":
         print(f"Your encrypted word is '{encrypt(text,shift)}'")
-    elif direction == "decrypt":
+    elif direction == "decode":
         print(f"Your decrypted word is '{decrypt(text,shift)}'")
     
     again = input("\nWould you like to encrypt or decrypt again (y or n?) ")
