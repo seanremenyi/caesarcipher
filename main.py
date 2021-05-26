4c1d9c8872f217afb9ad5afbc5d625d4d7dbc4e9
Sean <you@example.com>
2021-05-26 09:00:09 +0000

    finished cipher

--- main.py
+++ main.py
@@ -1,35 +1,44 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-def forward(letter, value):
-    if (letter + value) > 25:
-        return alphabet[(letter+value) - 26]
-    else:
-        return alphabet[letter + value]
+def shifting(letter, value, process):
+    if process == 1:
+        if (letter + value) > 25:
+            return alphabet[(letter+value) - 26]
+        else:
+            return alphabet[letter + value]
+    elif process == 2:
+        if (letter - value) < 0:
+            return alphabet[(letter - value) + 26]
+        else:
+            return alphabet[letter - value]
 
 def encrypt(text, shift):
     final = []
     for char in text:
         start = alphabet.index(char)
-        end = forward(start, shift)
+        end = shifting(start, shift, 1)
         final.append(end)
     return"".join(final)
-
-def reverse(letter, value):
-    if (letter - value) < 0:
-        return alphabet[(letter - value) + 26]
-    else:
-        return alphabet[letter - value]
 
 def decrypt(text, shift):
     final = []
     for char in text:
         start = alphabet.index(char)
-        end = reverse(start, shift)
+        end = shifting(start, shift, 2)
         final.append(end)
-    return"".join(final)
+    return "".join(final)
 
-print(decrypt(text,shift))
+while True:
+    direction = input("\nType 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    if direction == "encode":
+        print(f"Your encrypted word is '{encrypt(text,shift)}'")
+    elif direction == "decrypt":
+        print(f"Your decrypted word is '{decrypt(text,shift)}'")
+    
+    again = input("\nWould you like to encrypt or decrypt again (y or n?) ")
+    if again == "n":
+        break
+    
